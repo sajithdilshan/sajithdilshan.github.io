52fadb6928f3fd785a382e7713f159bb0452e62d
Sajith Dilshan <sajithdilshan@gmail.com>
2021-05-15 14:05:21 +0200

    adjust dates

---
+++ assets/js/date.js
@@ -0,0 +1,10 @@
+function adjustAge() {
+     document.getElementById("age").innerHTML = (new Date()).getFullYear() - (new Date(1990, 2, 26)).getFullYear();
+}
+
+function adjustCurrentYear() {
+     document.getElementById("currentYear").innerHTML = (new Date()).getFullYear();
+}
+
+adjustAge()
+adjustCurrentYear()
