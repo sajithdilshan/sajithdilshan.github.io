c2c16a186a39b5c3a0546ec16b66621e31b8ace9
Sajith Edirisinghe <sajithdilshan@gmail.com>
2024-08-04 16:12:06 +0200

    update page

--- assets/js/date.js
+++ assets/js/date.js
@@ -1,10 +1,5 @@
-function adjustAge() {
-    document.getElementById("age").innerHTML = (new Date()).getFullYear() - (new Date(1990, 2, 26)).getFullYear();
-}
-
 function adjustCurrentYear() {
     document.getElementById("currentYear").innerHTML = (new Date()).getFullYear();
 }
 
-adjustAge()
 adjustCurrentYear()
